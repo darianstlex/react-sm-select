7912d1c6dc7624a418bc3ce67117e371b1e0bc45
Ivan Uzun <ivan.uzun@travelex.com>
2018-10-04 09:40:26 +0100

    utils docs

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,16 @@
+/**
+ * Checks if target is of given type
+ * @param type
+ * @param target
+ * @returns {boolean}
+ */
 export const is = (type, target) => target.constructor === type;
 
+/**
+ * Compose classes
+ * @param args
+ * @returns {*|string}
+ */
 export const classes = (...args) => args.reduce((acc, item) => {
   let addOn = '';
   if (is(String, item)) addOn = item;
@@ -8,38 +19,73 @@ export const classes = (...args) => args.reduce((acc, item) => {
   return (acc ? `${acc} ${addOn}` : addOn).trim();
 }, '');
 
+/**
+ * Filter array by containing text in elements
+ * @param options
+ * @param filter
+ * @returns {*}
+ */
 export const defaultFilterOptions = (options, filter) =>
   options.filter(option =>
     option.label.toLowerCase().includes(filter.toLowerCase())
   );
 
+/**
+ * Checks if two arrays are identical
+ * @param first
+ * @param second
+ * @returns {boolean}
+ */
 export const areArraysEqual = (first, second) => {
   if (first.length !== second.length) return false;
   return !first.reduce((A, item, idx) => item !== second[idx] ? [...A, item] : A, []).length
 };
 
+/**
+ * Clear array from props doesn't exist in second array
+ * @param origin
+ * @param part
+ * @param single
+ * @returns {*}
+ */
 export const omitDirtyValues = (origin, part, single) => {
   const flatOrigin = origin.map(item => item.value);
   const [first, ...rest] = part.reduce((A, item) => flatOrigin.includes(item) ? [...A, item] : A, []);
   return first ? (single ? [first] : [first, ...rest]) : [];
 };
 
-
+/**
+ * Attaches listener callback to document touch ot mousedown
+ * @param cb
+ */
 export const attachDocumentClickListener = (cb) => {
   document.addEventListener('touchstart', cb, false);
   document.addEventListener('mousedown', cb, false);
 };
 
+/**
+ * Removes listener from document touch ot mousedown
+ * @param cb
+ */
 export const removeDocumentClickListener = (cb) => {
   document.removeEventListener('touchstart', cb, false);
   document.removeEventListener('mousedown', cb, false);
 };
 
+/**
+ * Stops event propagation
+ * @param event
+ */
 export const stopPreventPropagation = event => {
   event.stopPropagation();
   event.preventDefault();
 };
 
+/**
+ * Calculate element path for browser compatibility
+ * @param event
+ * @returns {*}
+ */
 export const eventPath = event => {
   const inPath = (event.composedPath && event.composedPath()) || event.path;
   const target = event.target;
