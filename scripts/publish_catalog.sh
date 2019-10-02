6427fb678c7a7ce2d562e8439be1c5b7ca73a370
darianstlex <ivan.uzun@travelex.com>
2019-10-02 10:03:23 +0100

    fix catalog deploy script

--- scripts/publish_catalog.sh
+++ scripts/publish_catalog.sh
@@ -4,7 +4,7 @@ set -e # exit with nonzero exit code if anything fails
 
 # get GIT url
 
-GIT_URL=`git config --get remote.origin.url`
+GIT_URL=$(git config --get remote.origin.url)
 if [[ ${GIT_URL} == "" ]]; then
   echo "This project is not configured with a remote git repo".
   exit 1
@@ -35,10 +35,10 @@ git commit -m "Deploy Catalog to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet ${GIT_URL} master:gh-pages > /dev/null 2>&1
-#git push --force ${GIT_URL} master:gh-pages
+git push --force --quiet "${GIT_URL}" master:gh-pages > /dev/null 2>&1
+
 cd ..
 rm -rf .tmp
 
 echo ""
-echo "=> Storybook deployed to: `node scripts/getUrl.js $GIT_URL`"
+echo "=> Catalog deployed to: $(node scripts/getUrl.js "$GIT_URL")"
